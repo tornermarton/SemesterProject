aab06c3ee8d6d75f0c2f75bfb80b2299425482b9
tornermarton <torner.marton@gmail.com>
2019-04-14 23:49:33 +0200

    Added read_file_list

--- functions.py
+++ functions.py
@@ -1,2 +1,21 @@
 from market_features import *
 from market_plots import *
+
+import os
+
+def read_file_list(data_root_dir, verbose=False):
+    updates_file_list = []
+    snapshots_file_list = []
+
+    for (dirpath, dirnames, filenames) in os.walk(data_root_dir):
+        updates_file_list.extend([dirpath+'/'+filename for filename in filenames if filename != ".DS_Store" and filename[0] == 'u'])
+        snapshots_file_list.extend([dirpath+'/'+filename for filename in filenames if filename != ".DS_Store" and filename[0] == 's'])
+
+    updates_file_list = sorted(updates_file_list)
+    snapshots_file_list = sorted(snapshots_file_list)
+    
+    if verbose:
+        print(len(snapshots_file_list), "update files read.")
+        print(len(updates_file_list), "snapshot files read.")
+        
+    return updates_file_list, snapshots_file_list
