5b0ebfda313f18d15567caa839c8691d17609777
tornermarton <torner.marton@gmail.com>
2019-04-25 21:40:20 +0200

    Some visualization functions changed, labeling changed

--- functions/market/plots.py
+++ functions/market/plots.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
+import matplotlib.patches as patches
 
 from functions.market.features import *
 
@@ -83,21 +83,24 @@ def plot_price_movement(dataset):
     plt.show()
 
 
-def plot_labels(wamps, labels):
+def plot_labels(values, labels):
     fig, ax = plt.subplots()
 
-    markers = ['ro' if label == -1 else 'go' if label == 1 else 'y.' for label in labels]
+    ax.plot(values)
 
-    for i, wamp in enumerate(wamps):
-        ax.plot(i, wamp, markers[i])
+    for i, label in enumerate(labels):
+        if label == 1:
+            ax.axvspan(i-0.5, i+0.5, alpha=0.3, color="green", lw=0)
+        elif label == -1:
+            ax.axvspan(i-0.5, i+0.5, alpha=0.3, color="red", lw=0)
 
     ax.set_ylabel('Price')
     ax.set_xlabel('Time')
     ax.set_title('Daily price movement')
 
-    ax.legend([Line2D([0], [0], marker='o', color='g', label='UP', markersize=15),
-               Line2D([0], [0], marker='.', color='y', label='NO_MOVE', markersize=15),
-               Line2D([0], [0], marker='o', color='r', label='DOWN', markersize=15), ],
+    ax.legend([patches.Rectangle((0, 0), 1, 1, facecolor='green'),
+               patches.Rectangle((0, 0), 1, 1, facecolor='white'),
+               patches.Rectangle((0, 0), 1, 1, facecolor='red')],
               ('UP', 'NO_MOVE', 'DOWN'))
 
     plt.show()
--- functions/preprocessing.py
+++ functions/preprocessing.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 
-from functions.market.features import calc_spread, calc_WAMP
+from functions.market.features import *
 
 __all__ = [
     "generate_binary_label",
@@ -22,7 +22,7 @@ def generate_binary_label(history_mean, future_mean):
 
 
 def generate_trinary_label(history_mean, future_mean, threshold_base, alpha):
-    threshold = threshold_base*alpha
+    threshold = threshold_base * alpha
 
     if future_mean - history_mean < -threshold:
         return -1
@@ -32,48 +32,51 @@ def generate_trinary_label(history_mean, future_mean, threshold_base, alpha):
         return 0
 
 
-def make_label(snapshot, future_snapshots, add_no_move=False, alpha=1):
-    spread = calc_spread(snapshot)
-    wamp = calc_WAMP(snapshot)
+def make_label(history_snapshots, future_snapshots, add_no_move=False, alpha=1):
+    history_vwaps = [calc_VWAP(ss) for ss in history_snapshots]
+    future_vwaps = [calc_VWAP(ss) for ss in future_snapshots]
 
-    mean = np.mean([calc_WAMP(ss) for ss in future_snapshots])
+    history_mean = np.mean(history_vwaps)
+    future_mean = np.mean(future_vwaps)
+
+    volatility = calc_volatility(np.concatenate((history_vwaps, future_vwaps)))
 
     if add_no_move:
-        return generate_trinary_label(wamp, mean, spread, alpha)
+        return generate_trinary_label(history_mean, future_mean, volatility, alpha)
     else:
-        return generate_binary_label(wamp, mean)
+        return generate_binary_label(history_mean, future_mean)
 
 
-def make_labels(snapshots, add_no_move=False, alpha=1, delay=100):
-    if delay < 1:
+def make_labels(snapshots, add_no_move=False, alpha=1, window_size=100):
+    if window_size < 1:
         raise AttributeError()
 
-    y = np.zeros(len(snapshots)-delay)
+    y = np.zeros(len(snapshots) - 2 * window_size)
 
-    for i in range(len(y)):
-        snapshot = snapshots[i]
-        future_snapshots = snapshots[i+1:i+delay+1]
+    for i in range(window_size, len(snapshots) - window_size):
+        history_snapshots = snapshots[i - window_size:i]
+        future_snapshots = snapshots[i:i + window_size]
 
-        y[i] = make_label(snapshot, future_snapshots,  add_no_move, alpha)
+        y[i - window_size] = make_label(history_snapshots, future_snapshots, add_no_move, alpha)
 
     return y
 
 
-def count_labels(dataset):
+def count_labels(labels):
     up_cnt = 0
     down_cnt = 0
     nomove_cnt = 0
-    for i,data in enumerate(dataset):
-        if data["label"] == -1:
+    for label in labels:
+        if label == -1:
             down_cnt += 1
-        if data["label"] == 0:
+        if label == 0:
             nomove_cnt += 1
-        if data["label"] == 1:
+        if label == 1:
             up_cnt += 1
 
-    print("UP labels:", up_cnt, '\t', '{}%'.format(up_cnt/(up_cnt+nomove_cnt+down_cnt)*100))
-    print("NO_MOVE labels:", nomove_cnt, '\t', '{}%'.format(nomove_cnt/(up_cnt+nomove_cnt+down_cnt)*100))
-    print("DOWN labels:", down_cnt, '\t', '{}%'.format(down_cnt/(up_cnt+nomove_cnt+down_cnt)*100))
+    print("UP labels:", up_cnt, '\t', '{}%'.format(up_cnt / (up_cnt + nomove_cnt + down_cnt) * 100))
+    print("NO_MOVE labels:", nomove_cnt, '\t', '{}%'.format(nomove_cnt / (up_cnt + nomove_cnt + down_cnt) * 100))
+    print("DOWN labels:", down_cnt, '\t', '{}%'.format(down_cnt / (up_cnt + nomove_cnt + down_cnt) * 100))
 
 
 def read_file_list(data_root_dir, verbose=False):
@@ -81,14 +84,16 @@ def read_file_list(data_root_dir, verbose=False):
     snapshots_file_list = []
 
     for (dirpath, dirnames, filenames) in os.walk(data_root_dir):
-        updates_file_list.extend([dirpath+'/'+filename for filename in filenames if filename != ".DS_Store" and filename[0] == 'u'])
-        snapshots_file_list.extend([dirpath+'/'+filename for filename in filenames if filename != ".DS_Store" and filename[0] == 's'])
+        updates_file_list.extend(
+            [dirpath + '/' + filename for filename in filenames if filename != ".DS_Store" and filename[0] == 'u'])
+        snapshots_file_list.extend(
+            [dirpath + '/' + filename for filename in filenames if filename != ".DS_Store" and filename[0] == 's'])
 
     updates_file_list = sorted(updates_file_list)
     snapshots_file_list = sorted(snapshots_file_list)
-    
+
     if verbose:
         print(len(snapshots_file_list), "update files read.")
         print(len(updates_file_list), "snapshot files read.")
-        
+
     return updates_file_list, snapshots_file_list
